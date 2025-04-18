b1f9e85d7bbc10c488071ceb770a6770d57eead2
Your Name <you@example.com>
2025-04-18 15:12:34 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -55,7 +55,7 @@ HashMap * createMap(long capacity) {
     mapa->size = 0;
     mapa->capacity = capacity;
     mapa->current = 0;
-    return NULL;
+    return mapa;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
