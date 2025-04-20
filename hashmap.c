29cf3a80468d78a38d1de6bd5118d4d9a021d832
Your Name <you@example.com>
2025-04-20 10:19:46 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -78,6 +78,8 @@ Pair * firstMap(HashMap * map){
     }
     if(map->current == map->capacity){
         return NULL; // No hay elementos en la tabla hash
+    }else{
+        return map->buckets[map->current];
     }
 
 }
