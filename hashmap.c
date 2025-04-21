7d84a6873b16f15a91d651d4b1318caf5daa9d76
Your Name <you@example.com>
2025-04-20 23:26:50 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -74,14 +74,13 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map){
-    map->current = 0;
-    while(map->current < map->capacity && map->buckets[map->current] == NULL){
-        map->current++;
+    for (int i = 0; i < map->capacity; i++) {
+        if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
+            map->current = i;
+            return map->buckets[i];
+        }
     }
-    if(map->current == map->capacity){
-        return NULL; // No hay elementos en la tabla hash
-    }
-    return map->buckets[map->current] ->value;
+    return NULL;
 
 }
 Pair * nextMap(HashMap * map) {
