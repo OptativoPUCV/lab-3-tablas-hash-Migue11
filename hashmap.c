508ad4b829e2468f4fd5379eca21537bab6885d5
Your Name <you@example.com>
2025-04-20 23:51:42 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -81,6 +81,7 @@ Pair * searchMap(HashMap * map,  char * key){
     int index = hash(key, map->capacity);
     while(map->buckets[index] != NULL){
         if(map->buckets[index]->key != NULL && is_equal(map->buckets[index]->key, key)){
+            map->current = index;
             return map->buckets[index];
         }
         index = (index + 1) % map->capacity;
